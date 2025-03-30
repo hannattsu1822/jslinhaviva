19fdd1df0589d8b92fa1378250d27cde4576b274
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-03-30 20:51:26 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(session({
 
 // Configuração do pool de conexões MySQL
 const pool = mysql.createPool({
-  host: '10.116.0.2',
+  host: '127.0.0.1',
   user: 'root',
   password: '289956Hg@#nhm', // Substitua pela sua senha
   database: 'banco_linha', // Substitua pelo nome do seu banco
