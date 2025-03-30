9368f160045c2d0b1432f87b91c3d4e80aae4b96
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-03-30 20:41:18 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,11 @@
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  user: 'root',
+  host: '127.168.0.1',
+  database: 'banco_linha',
+  password: '289956Hg@#nhm',
+  port: 33006,
+});
+
+module.exports = pool;
