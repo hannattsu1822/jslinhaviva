1f1fddecad0fab5331e6325e671077fffec47d7f
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-03-30 20:08:13 -0300

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: '159.223.147.154',
+  user: 'root',
+  password: 'SUA_SENHA_AQUI', // Substitua pela sua senha
+  database: 'banco_linha',
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Erro ao conectar ao banco:', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL!');
+});
+
+module.exports = connection;
