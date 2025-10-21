e5d517d6075f10c95e94e5acf743d48adfbc80c2
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-10-21 11:02:07 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,17 +1,13 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js");
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyB9VxZXQNoV_Uxf0JhMybv145e7nAGBjJA",
+  authDomain: "gestao-servicos-notifica-18299.firebaseapp.com",
+  projectId: "gestao-servicos-notifica-18299",
+  storageBucket: "gestao-servicos-notifica-18299.appspot.com",
+  messagingSenderId: "635720711489",
+  appId: "1:635720711489:web:42347140989c527bf335c0"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -23,7 +19,7 @@ messaging.onBackgroundMessage(function (payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/static/images/firebase-logo.png", // Crie um ícone se desejar
+    icon: "/static/images/firebase-logo.png", 
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
