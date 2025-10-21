f1bb788d6c33baa5c759e992319def49a1f158ab
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-10-21 10:44:29 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const { WebSocketServer } = require("ws");
 const { app } = require("./init");
 const { iniciarClienteMQTT } = require("./mqtt_handler");
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../gestao-servicos-notifica-18299-firebase-adminsdk-fbsvc-f88a4cba28.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+console.log("Firebase Admin SDK inicializado com sucesso.");
+
 const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
@@ -19,7 +29,6 @@ wss.on("connection", (ws) => {
 
 iniciarClienteMQTT(app);
 
-
 const aggregatorRoutes = require("./routes");
 app.use("/", aggregatorRoutes);
 
@@ -27,5 +36,3 @@ const port = process.env.SERVER_PORT || 3000;
 server.listen(port, () => {
   console.log(`Servidor HTTP e WebSocket rodando em http://localhost:${port}`);
 });
-
-
