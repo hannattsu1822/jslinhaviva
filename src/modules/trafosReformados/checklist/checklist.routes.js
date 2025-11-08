bf23c17027df91912090d8e812b88fb788cfeb58
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-11-08 09:40:56 -0300

    Update checklist.routes.js

--- src/modules/trafosReformados/checklist/checklist.routes.js
+++ src/modules/trafosReformados/checklist/checklist.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { autenticar, verificarNivel } = require("../../../auth");
 const controller = require("./checklist.controller");
+const { uploadAnexoChecklist } = require("../../../init");
 
 router.get(
   "/api/checklist_por_registro/:registroId",
@@ -9,24 +10,29 @@ router.get(
   verificarNivel(3),
   controller.obterChecklistPorRegistroId
 );
+
 router.get(
   "/api/historico_por_serie/:numero_serie",
   autenticar,
   verificarNivel(3),
   controller.obterHistoricoPorSerie
 );
+
 router.put(
   "/api/transformadores_reformados/:id/avaliar_completo",
   autenticar,
   verificarNivel(3),
+  uploadAnexoChecklist.single("anexo_imagem"),
   controller.avaliarCompleto
 );
+
 router.post(
   "/api/gerar_pdf_checklist_especifico",
   autenticar,
   verificarNivel(3),
   controller.gerarPdfChecklist
 );
+
 router.post(
   "/api/gerar_pdf_tabela_historico",
   autenticar,
